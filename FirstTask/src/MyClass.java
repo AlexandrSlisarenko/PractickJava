2d555ae809c35299f715cad7d4cdcccee41166d0
Alexandr Slisarenko <slicarenko@gmail.com>
2018-09-06 22:38:31 +0300

    Begin

---
+++ FirstTask/src/MyClass.java
@@ -0,0 +1,9 @@
+public class MyClass {
+    public static void main(String args[]){
+        System.out.println("Hello World!!!");
+        float volume;
+        float radius = 2.0f , height = 10.0f;
+        volume = 3.1416f * radius * radius * height;
+        System.out.println("Объем цилиндра равен "+ volume);
+    }
+}
