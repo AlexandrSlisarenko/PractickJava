50d5074e731def64ab8673739f556d636b2751de
Alexandr Slisarenko <slicarenko@gmail.com>
2018-09-09 13:15:25 +0300

    Массивы и Цыклы 4/7

--- ArraysAndCycles/src/com/company/Main.java
+++ ArraysAndCycles/src/com/company/Main.java
@@ -1,11 +1,18 @@
 package com.company;
 
+import sun.awt.geom.AreaOp;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         int [] arr = {1,0,0,1,1,0,0,0,1,0,1};
         //replaceZerobyone(arr);
+        //arr = new int [8];
+        //fillingAnArray(arr);
+        //lessThenSixMultiplyByTwo();
+        int [] arr1 = {23,5,1,6,0,45,3,2,78};
+        minMax(arr1);
     }
 
    static void replaceZerobyone(int [] arr){
@@ -13,13 +20,48 @@ public class Main {
 
         System.out.println("Начальный массива");
         for(int i = 0;i < l;i++){
-            System.out.println(arr[i]);
+            System.out.print(arr[i]);
         }
         System.out.println("Результат");
        for(int i = 0;i < l;i++){
            if(arr[i] == 0){arr[i] = 1;}else{arr[i] = 0;}
-           System.out.println(arr[i]);
+           System.out.print(arr[i]);
        }
 
+   }
+
+   static void fillingAnArray(int [] arr){
+        int i  = 0;
+        int j = 0;
+        int l = arr.length;
+        while (i < l){
+
+            arr[i] = j;
+            System.out.println(arr[i]);
+            j += 3;
+            i++;
+        }
+   }
+
+   static void lessThenSixMultiplyByTwo(){
+        int i = 0;
+        int [] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        do{
+
+            if(arr[i] < 6) arr[i] *=2;
+            System.out.println(arr[i]);
+            i++;
+        }while(i < arr.length);
+   }
+
+   static void minMax(int [] arr){
+        int min = arr[0];
+        int max = arr[0];
+        for (int i = 1; i <arr.length; i++){
+            if(arr[i] < min) min = arr[i];
+            if(arr[i] > max) max = arr[i];
+        }
+        System.out.println(min);
+        System.out.println(max);
    }
 }
