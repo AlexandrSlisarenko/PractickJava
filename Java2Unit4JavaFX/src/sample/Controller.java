7fcc909cf3070bce3b4792035bd7b6c7f8b6062e
Alexandr Slisarenko <slicarenko@gmail.com>
2019-05-28 19:40:02 +0300

    Первоначальный вид чата

--- Java2Unit4JavaFX/src/sample/Controller.java
+++ Java2Unit4JavaFX/src/sample/Controller.java
@@ -3,8 +3,10 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 
 public class Controller {
     @FXML
@@ -13,11 +15,27 @@ public class Controller {
     TextField jtf;
     @FXML
     Button jb1;
+    @FXML
+    ScrollPane jscroll;
+    @FXML
+    VBox jboxMSG;
 
     public void sendMSG(ActionEvent actionEvent) {
 
-        jta.appendText(jtf.getText() + "\n");
-        jtf.setText("");
+        TextArea ta = new TextArea();
+        ta.appendText(jtf.getText());
+        ta.autosize();
+        ta.setPrefHeight(50);
+        ta.setEditable(false);
+
+        if(((jboxMSG.getChildren().size()+1)%2) == 0){
+            ta.getStyleClass().add("tabUser");
+            System.out.println(ta.getStyleClass());
+
+
+        }
+        ta.getStyleClass().add("msg");
+        jboxMSG.getChildren().add(ta);
 
 
     }
